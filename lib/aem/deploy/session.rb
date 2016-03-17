39fffdb26536fdf2095a8b7786c95a64248f5c8a
Mitch Eaton <meaton@mitchs-mbp.us.ad.westfield.com>
2016-03-16 17:03:48 -0700

    first run

--- lib/aem/deploy/session.rb
+++ lib/aem/deploy/session.rb
@@ -2,9 +2,6 @@ require 'rest_client'
 require 'open-uri'
 require 'uri'
 require 'cgi'
-require 'yaml'
-require 'rake'
-require 'erb'
 require 'json'
 require 'pry'
 module Aem::Deploy
@@ -17,7 +14,7 @@ module Aem::Deploy
         @host = options.fetch(:host)
         @user = options.fetch(:user)
         @pass = CGI.escape(options.fetch(:pass))
-        @retry = options.fetch(:retry) unless options[:retry].nil? 
+        @retry = options.fetch(:retry) unless options[:retry].nil?
       else
         raise 'Hostname, User and Password are required'
       end
@@ -25,9 +22,9 @@ module Aem::Deploy
 
     # Install latest package to CMS
     def install_package(package_path)
-      upload = RestClient.post("http://#{@user}:#{@pass}@#{@host}/crx/packmgr/service/.json", :cmd => 'upload', :package => File.new(package_path, 'rb'), :timeout => 300)
+      upload = RestClient.post("http://#{@user}:#{@pass}@#{@host}/crx/packmgr/service/.json", :cmd => 'upload', :package => File.new(package_path, 'rb'), :force => true, :timeout => 300)
       parse_response(upload)
-      upload_path = JSON.parse(upload)["path"]
+      upload_path = URI.encode(JSON.parse(upload)["path"])
       install = RestClient.post("http://#{user}:#{pass}@#{host}/crx/packmgr/service/.json#{upload_path}", :cmd => 'install', :timeout => 300)
       parse_response(install)
     rescue RestClient::RequestTimeout => error
@@ -56,11 +53,11 @@ module Aem::Deploy
     # Checks response of any request to CMS. Breaks script if unexpected response.
     def parse_response(message)
       if JSON.parse(message)['success'] == true
-        puts "  #{message}"
+        return "  #{message}"
       elsif JSON.parse(message)['msg'].include?("Package already exists")
-        puts "  #{message}"
+        return "  #{message}"
       elsif message.include? ("302 Found")
-        puts '  JSPs Recompiled'
+        return '  JSPs Recompiled'
       else
         raise "  It looks there was a problem uploading/installing the package #{JSON.parse(message)}"
       end
